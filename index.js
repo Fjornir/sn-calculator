e0ff7f8f040a041ff539f5742aa181ab5491717b
Nikita <50362451+Fjornir@users.noreply.github.com>
2023-04-19 16:31:33 +0300

    Handle some breaking logic, simple validation, keyboard support

--- index.js
+++ index.js
@@ -26,18 +26,78 @@ class Calculator {
   }
 }
 
+function refreshFirstValue(operation, res) {
+  switch (operation) {
+    case "+":
+      calc.setFirstValue(res + "+");
+      break;
+    case "-":
+      calc.setFirstValue(res + "-");
+      break;
+    case "*":
+      calc.setFirstValue(res + "*");
+      break;
+    case "/":
+      calc.setFirstValue(res + "/");
+      break;
+  }
+}
+
+function handleOperation(mark) {
+  console.log("ya tyt");
+  if (calc.calcNode.value.match(regexSec)) {
+    if (isOperationBtnPressed) {
+      calc.setFirstValue(calc.firstValue.slice(0, -1) + mark);
+      operation = mark;
+    } else {
+      calc.setFirstValue(calc.calcNode.value + mark);
+      operation = mark;
+      console.log("Event target value " + mark);
+      calc.calcNode.value = null;
+      isSecondNeedToUpdate = true;
+      isOperationBtnPressed = true;
+    }
+
+    console.log(calc.firstValue);
+    console.log(isOperationBtnPressed);
+  }
+}
+
+function handleEquals() {
+  if (calc.calcNode.value.match(regexSec)) {
+    if (isSecondNeedToUpdate) {
+      calc.setSecondValue(calc.calcNode.value);
+      isSecondNeedToUpdate = false;
+    }
+
+    history.push(`<div>${calc.firstValue + calc.secondValue}</div>`);
+    operationsDisplay.innerHTML = history.join("");
+    operationsDisplay.scrollTop = operationsDisplay.scrollHeight;
+    let res = eval(calc.firstValue + calc.secondValue);
+
+    refreshFirstValue(operation, res);
+
+    calc.calcNode.value = res;
+    isOperationComplete = true;
+    isNumbersNeedToReset = true;
+    isOperationBtnPressed = false;
+    input.focus();
+  }
+}
+
 const input = document.querySelector(".calculator__input");
 const btns = document.querySelectorAll(".calculator-numbers__item");
 const equalsBtn = document.querySelector(".calculator-numbers__equals");
 const operationButtons = document.querySelectorAll(
   ".calculator-numbers__operation"
 );
-let operationsDisplay = document.querySelector(".operations")
-
+let operationsDisplay = document.querySelector(".operations");
+let history = [];
 let operation = "";
 let isOperationComplete = false;
 let isSecondNeedToUpdate = false;
 let isNumbersNeedToReset = false;
+let isOperationBtnPressed = false;
 
 const calc = new Calculator(input);
 
@@ -56,45 +116,13 @@ btns.forEach((item) => {
 });
 
 operationButtons.forEach((item) => {
-  item.addEventListener("click", function (event) {
-    if (calc.calcNode.value.match(regexSec)) {
-      calc.setFirstValue(calc.calcNode.value + event.target.value);
-      operation = event.target.value;
-      calc.calcNode.value = null;
-      isSecondNeedToUpdate = true;
-    }
+  item.addEventListener("click", function (e) {
+    handleOperation(e.target.value);
   });
 });
 
-equalsBtn.addEventListener("click", function (event) {
-  if (calc.calcNode.value.match(regexSec)) {
-    if (isSecondNeedToUpdate) {
-      calc.setSecondValue(calc.calcNode.value);
-      isSecondNeedToUpdate = false;
-    }
-    console.log(calc.firstValue + calc.secondValue);
-    console.log(operationsDisplay);
-    operationsDisplay.innerHTML = calc.firstValue + calc.secondValue;
-    let res = eval(calc.firstValue + calc.secondValue);
-
-    switch (operation) {
-      case "+":
-        calc.setFirstValue(res + "+");
-        break;
-      case "-":
-        calc.setFirstValue(res + "-");
-        break;
-      case "*":
-        calc.setFirstValue(res + "*");
-        break;
-      case "/":
-        calc.setFirstValue(res + "/");
-        break;
-    }
-    calc.calcNode.value = res;
-    isOperationComplete = true;
-    isNumbersNeedToReset = true;
-  }
+equalsBtn.addEventListener("click", function () {
+  handleEquals();
 });
 
 input.onclick = function () {
@@ -102,4 +130,33 @@ input.onclick = function () {
     calc.calcNode.value = null;
     isOperationComplete = false;
   }
+};
+
+$(".calculator-numbers__clear").click(function (e) {
+  calc.calcNode.value = null;
+});
+
+input.onkeydown = function (event) {
+  switch (event.keyCode) {
+    case 107:
+      handleOperation("+");
+      break;
+    case 109:
+      handleOperation("-");
+      break;
+    case 106:
+      handleOperation("*");
+      break;
+    case 111:
+      handleOperation("/");
+      break;
+    case 13:
+      handleEquals();
+      break;
+    default:
+      break;
+  }
+
+  console.log(event.keyCode);
+  console.log("calc.firstValue = " + calc.firstValue);
 };
